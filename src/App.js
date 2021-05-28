805da514ad7b031c5dce182beb4d6ab15d2dd75d
007Abdullah <kb337137@gmail.com>
2021-05-28 16:43:49 +0500

    chart js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, TextField, Button } from '@material-ui/core';
+import Navbar from './Navbar';
+import VerticalBar from './Barchart';
+import PieChart from './Pie';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    height: theme.spacing(90),
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+  paper: {
+    width: 300,
+    height: 400,
+    marginTop: 50,
+    margin: '0 auto',
+    textAlign: 'center'
+  },
+  Loginform: {
+    marginTop: theme.spacing(10)
+  }
+}));
+
+
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <br />
+      <PieChart />
+      <br />
+      <VerticalBar />
+      <br />
+      <div className={classes.root}>
+        <Paper elevation={3} className={classes.paper}>
+          <form className={classes.Loginform} autoComplete="off">
+            <h1>Login</h1>
+            <TextField id="standard-basic" label="Email" />
+            <br />
+            <br />
+            <TextField id="standard-basic" label="Password" />
+            <br />
+            <br />
+            <Button variant="contained" color="primary">
+              Login
+            </Button>
+          </form>
+        </Paper>
+      </div>
+    </React.Fragment>
   );
 }
 
